5a7e02725782880db71bf9e4d9b90f2fb1cc5976
Daniel Radd <draddx@outlook.com>
2017-06-18 23:53:53 -0300

    Primeira Versão para Teste
    Modelo simples de adição de convidados para uma lista de festa.

---
+++ demo-gestao-festa/src/main/java/com/draddx/festa/DemoGestaoFestaApplication.java
@@ -0,0 +1,12 @@
+package com.draddx.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGestaoFestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGestaoFestaApplication.class, args);
+	}
+}
